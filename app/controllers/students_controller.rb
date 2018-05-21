5cc34cef38badf221c4e428d7ac5bd63c85d940f
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-21 09:58:58 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create 
+    @student = Student.create(params_hash)
+
+    redirect_to students_path
+  end
+
+  private def params_hash
+    params.require(:student).permit(:first_name, :last_name)
+  end
+  
+
 end
